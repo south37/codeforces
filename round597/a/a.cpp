cfc87ab36bd216fd47aa2c58b0449d9b87a37db6
Nao Minami <south37777@gmail.com>
2019-11-01 23:45:45 +0900

    Add a

--- round597/a/a.cpp
+++ round597/a/a.cpp
@@ -28,9 +28,36 @@ typedef double D;
 const ll INF = 1e9;
 const ll MOD = 1000000007;  // 1e9 + 7
 
-int main(int argc, char** argv) {
-  int N;
-  cin >> N;
+ll gcd(ll a, ll b) {
+  if (a < b) { swap(a, b); }
+  if (b == 0) { return a; }
+  return gcd(b, a % b);
+}
 
-  cout << N << endl;
+void solve() {
+  ll a, b;
+  cin >> a >> b;
+  if ((a == 1) || (b == 1)) {
+    cout << "Finite" << endl;
+    return;
+  }
+
+  // Here, a > 1 and b > 1.
+  // We want to check the prime ness of a and b
+  ll g = gcd(a, b);
+  if (g > 1) { // there are numbers which is not represetatable.
+    cout << "Infinite" << endl;
+    return;
+  } else { // g == 1
+    cout << "Finite" << endl;
+    return;
+  }
+}
+
+int main(int argc, char** argv) {
+  ll t;
+  cin >> t;
+  rep(i, t) {
+    solve();
+  }
 }
