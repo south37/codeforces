858cd185eceae050dc4d65d4541e627423850ce3
Nao Minami <south37777@gmail.com>
2019-11-02 01:36:17 +0900

    Add d

--- round597/d/d.cpp
+++ round597/d/d.cpp
@@ -29,8 +29,76 @@ const ll INF = 1e9;
 const ll MOD = 1000000007;  // 1e9 + 7
 
 int main(int argc, char** argv) {
-  int N;
-  cin >> N;
+  ll n;
+  cin >> n;
+  vector<ll> xs(n);
+  vector<ll> ys(n);
+  rep(i, n) {
+    ll x, y;
+    cin >> xs[i] >> ys[i];
+  }
+  vector<ll> c(n); // the power station.
+  rep(i, n) {
+    cin >> c[i];
+  }
+  vector<ll> k(n); // the cost of wire.
+  rep(i, n) {
+    cin >> k[i];
+  }
 
-  cout << N << endl;
+  ll s = 0; // The sum of building power station.
+  rep(i, n) {
+    s += c[i];
+  }
+
+  // nearest[i] .. the sorted map of points from i.
+  vector< map<P, ll> > nearests(n);
+  rep(i, n) {
+    rep(j, n) {
+      if (j == i) { continue; }
+      ll dist = abs(xs[i] - xs[j]) + abs(ys[i] - ys[j]);
+      P p(dist, j);
+      nearests[i][p] = j;
+    }
+  }
+  // Here, nearest[i] is sorted map. { dist => i }. increasing
+
+  // For Debug
+  // rep(i, n) {
+  //   cout << "i: " << i << endl;
+  //   for (auto x : nearests[i]) {
+  //     cout << "j: " << x.second << ", dist: " << x.first.first << endl;
+  //   }
+  // }
+
+  vector<P> wires;
+  vector<ll> power_indices;
+
+  // Check if wire or power station
+  rep(i, n) {
+    auto it = nearests[i].begin();
+    ll j = (*it).second; // nearest index.
+    ll dist = (*it).first.first;
+    ll wire_cost = dist * (k[i] * k[j]);
+    if (c[i] > wire_cost) {
+      // Use wire
+      wires.emplace_back(i, j);
+      s -= c[i];
+      s += wire_cost;
+    } else {
+      power_indices.push_back(i);
+    }
+  }
+
+  cout << s << endl;
+  cout << power_indices.size() << endl;
+  for (auto x : power_indices) {
+    cout << x + 1<< " ";
+  }
+  cout << endl;
+
+  cout << wires.size() << endl;
+  for (auto x : wires) {
+    cout << x.first + 1 << " " << x.second + 1 << endl;
+  }
 }
