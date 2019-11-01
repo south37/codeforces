19cb4b305352c661c636e499ac7fe87aa74fc186
Nao Minami <south37777@gmail.com>
2019-11-01 21:44:42 +0900

    Add round596/a

---
+++ round596/a/a.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+#include <string.h>
+#include <set>
+
+using namespace std;
+
+#define COUT(x) cout << #x << " = " << (x) << " (L" << __LINE__ << ")" << endl
+
+#define rep(i, n) for(int i = 0; i < n; ++i)
+#define all(s) s.begin(), s.end()
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> triple;
+typedef double D;
+
+const ll INF = 1e9;
+const ll MOD = 1000000007;  // 1e9 + 7
+
+int main(int argc, char** argv) {
+  int da, db;
+  cin >> da >> db;
+  if (da == db) {
+    cout << da << 1 << " " << db << 2 << endl;
+    return 0;
+  }
+  if (da + 1 == db) {
+    cout << da << " " << db << endl;
+    return 0;
+  }
+
+  cout << -1 << endl;
+  return 0;
+}
