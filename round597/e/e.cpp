1f9cec17ca86ea658aec0ea2275d8ad417c1431f
Nao Minami <south37777@gmail.com>
2019-11-04 16:46:26 +0900

    Add e.cpp

--- round597/e/e.cpp
+++ round597/e/e.cpp
@@ -45,9 +45,58 @@ typedef double D;
 const ll INF = 1e9;
 const ll MOD = 1000000007;  // 1e9 + 7
 
-int main(int argc, char** argv) {
-  ll N;
-  cin >> N;
+int grid[10][10];
 
-  cout << N << endl;
+int main(int argc, char** argv) {
+  rep(i, 10) {
+    rep(j, 10) {
+      cin >> grid[i][j];
+    }
+  }
+
+  // flat:
+  // 99, 98, ... 90
+  // 80, 81, ... 89
+  // .
+  // .
+  // .
+  //  0,  1, ...  9
+  int flat[10][10];
+  rep(i, 10) {
+    rep(j, 10) {
+      flat[i][j] = (9-i)*10 + ((i&1) ? j : 9-j);
+    }
+  }
+
+  int d = 1; // "->" at first
+  int x = 9; // vertical
+  int y = 0; // horizontal
+  vector<int> arr(100);
+  rep(i, 100) {
+    arr[i] = flat[x-grid[x][y]][y];
+
+    if (y+d == -1 || y+d == 10) { // at the edge of a row. (y,d) = (9,1) or (0,-1)
+      d *= -1;
+      --x;
+    } else {
+      y += d;
+    }
+  }
+
+  // printvec(arr);
+
+  vector<double> dp(100);
+  dp[99] = 0;
+  for (int i = 98; i >= 0; --i) {
+    dp[i] = 1;
+    int c = 0; // The count of summed dp.
+    for (int r = 1; r <= 6; ++r) { // try 6 times. [i+1, i+6]
+      if (i + r >= 100) { continue; }
+      dp[i] += min(dp[i+r], dp[arr[i+r]]) / 6;
+      ++c;
+    }
+    dp[i] = 6 * dp[i] / c;
+  }
+
+  cout << setprecision(10) << fixed << dp[0] << endl;
 }
