c742f47b3426c9c9118f136a8ec1545180ac1c30
Nao Minami <south37777@gmail.com>
2019-11-05 00:40:26 +0900

    Add round596/c

---
+++ round596/c/c.cpp
@@ -0,0 +1,71 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+#include <string.h>
+#include <set>
+
+using namespace std;
+
+#define COUT(x) cout << #x << " = " << (x) << " (L" << __LINE__ << ")" << endl
+
+template<class T> void printvec(const vector<T>& v) {
+  for (auto x : v) { cout << x << " "; }
+  cout << endl;
+}
+template<class T> void printtree(const vector< vector<T> >& tree) {
+  for (long long i = 0; i < tree.size(); ++i) {
+    cout << i + 1 << ": "; printvec(tree[i]);
+  }
+}
+template<class T, class U> void printmap(const map<T, U>& mp) {
+  for (auto x : mp) { cout << x.first << "=>" << x.second << endl; }
+}
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+#define rep(i, n) for(ll i = 0; i < n; ++i)
+#define all(s) s.begin(), s.end()
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> triple;
+typedef double D;
+
+const ll INF = 1e9;
+const ll MOD = 1000000007;  // 1e9 + 7
+
+int main(int argc, char** argv) {
+  ll n, p;
+  cin >> n >> p;
+
+  for (ll i = 1; i <= 31; ++i) { // 10**9 ~ 2 ** 30
+    ll k = n - p * i; // try k.
+    // cout << "k: " << k << endl;
+    if (k <= 0) { continue; } // We check only positive k
+
+    // Count to represent k.
+    ll cnt = 0;
+    while (k > 0) {
+      if (k & 1) { ++cnt; }
+      k >>= 1;
+    }
+
+    if (cnt <= i) {
+      cout << i << endl;
+      return 0;
+    }
+  }
+
+  cout << -1 << endl;
+  return 0;
+}
