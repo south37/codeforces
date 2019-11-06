8e773c8ce0494b9368e6a2f81848fdf102b86e7d
Nao Minami <south37777@gmail.com>
2019-11-06 23:30:05 +0900

    Add round596/f

---
+++ round596/f/f.cpp
@@ -0,0 +1,74 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+#include <string.h>
+#include <set>
+
+using namespace std;
+
+#define COUT(x) cout << #x << " = " << (x) << " (L" << __LINE__ << ")" << endl
+
+template<class T> void printvec(const vector<T>& v) {
+  for (auto x : v) { cout << x << " "; }
+  cout << endl;
+}
+template<class T> void printtree(const vector< vector<T> >& tree) {
+  for (long long i = 0; i < tree.size(); ++i) {
+    cout << i + 1 << ": ";
+    printvec(tree[i]);
+  }
+}
+template<class T, class U> void printmap(const map<T, U>& mp) {
+  for (auto x : mp) { cout << x.first << "=>" << x.second << endl; }
+}
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+#define rep(i, n) for(ll i = 0; i < n; ++i)
+#define all(s) s.begin(), s.end()
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> triple;
+typedef double D;
+
+const ll INF = 1e9;
+const ll MOD = 1000000007;  // 1e9 + 7
+
+vector<ll> p; // p[i] .. parent of i
+vector<ll> h; // h[i] .. height of i
+vector< vector<ll> > c; // c[i] .. children of i
+
+int dfs(int v) {
+  h[v] = 1;
+  rep(i, c[v].size()) {
+    h[v] = max(h[v], dfs(c[v][i])+1);
+  }
+  return h[v];
+}
+
+int main(int argc, char** argv) {
+  ll n;
+  cin >> n;
+  p.resize(n);
+  c.resize(n);
+  p[0] = -1;
+  for (int i = 1; i < n; ++i) {
+    cin >> p[i];
+    c[p[i]].push_back(i);
+  }
+  dfs(0);
+
+
+  cout << N << endl;
+}
