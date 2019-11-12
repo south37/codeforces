5ab4d0623dc5ca8ff04a813f55b981eea26c74a1
Nao Minami <south37777@gmail.com>
2019-11-12 23:03:28 +0900

    Add edu_75/a

---
+++ edu_75/a/a.cpp
@@ -0,0 +1,92 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+#include <string.h>
+#include <set>
+
+using namespace std;
+
+#define COUT(x) cout << #x << " = " << (x) << " (L" << __LINE__ << ")" << endl
+
+template<class T> void printvec(const vector<T>& v) {
+  for (auto x : v) { cout << x << " "; }
+  cout << endl;
+}
+template<class T> void printtree(const vector< vector<T> >& tree) {
+  for (long long i = 0; i < tree.size(); ++i) {
+    cout << i + 1 << ": "; printvec(tree[i]);
+  }
+}
+template<class T, class U> void printmap(const map<T, U>& mp) {
+  for (auto x : mp) { cout << x.first << "=>" << x.second << endl; }
+}
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+#define rep(i, n) for(ll i = 0; i < n; ++i)
+#define all(s) s.begin(), s.end()
+#define fr first
+#define sc second
+#define mp make_pair
+#define pb push_back
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> triple;
+typedef double D;
+
+const ll INF = 1e9;
+const ll MOD = 1000000007;  // 1e9 + 7
+
+void solve() {
+  string s;
+  cin >> s;
+  ll n = s.size();
+
+  // Run Length Encoding
+  // e.g. issii -> i1s2i2
+  vector< pair<char, ll> > cnts;
+  rep(i, n) {
+    ll cnt = 1;
+    while (i < n - 1 && s[i] == s[i+1]) {
+      ++cnt;
+      ++i;
+    }
+    cnts.emplace_back(s[i], cnt);
+  }
+
+  set<char> ans;
+  for (auto v : cnts) {
+    if (v.sc % 2 != 0) { // may broken.
+      ans.insert(v.fr);
+    }
+  }
+
+  for (char e : ans) {
+    cout << e;
+  }
+  cout << endl;
+}
+
+int main(int argc, char** argv) {
+  cin.tie(NULL);
+  cout.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  //cout << setprecision(10) << fixed;
+
+  ll t;
+  cin >> t;
+  rep(i, t) {
+    solve();
+  }
+}
