dc4945be2ab3d345c6d142414c6dd7e86a810662
Nao Minami <south37777@gmail.com>
2019-11-16 20:00:18 +0900

    Add edu76/f

--- edu_76/f/f.cpp
+++ edu_76/f/f.cpp
@@ -48,9 +48,10 @@ typedef double D;
 const ll INF = 1e9;
 const ll MOD = 1000000007;  // 1e9 + 7
 
-const ll V = 5000000; // V > pow(2, 15)
+const ll V = 5000000; // V > pow(2, 15) * n ~ 33,000 * 100 ~ 3,300,000
+// const ll V = 100000;
 
-ll lst[V]; // Contains the information of sequences. last value of Trie-tree is a key, information is a value.
+vector<ll> lst(V); // Contains the information of sequences. last value of Trie-tree is a key, information is a value.
 map<ll, ll> nxt[V]; // Trie-tree.
 ll t = 1; // The counter. incremented in get_nxt.
 
@@ -60,10 +61,13 @@ ll get_nxt(ll v, ll x) {
   if (!nxt[v].count(x)) {
     nxt[v][x] = t++;
   }
+  // cout << "v: " << v << endl;
+  // cout << "x: " << x << endl;
+  // cout << "nxt[v][x]: " << nxt[v][x] << endl;
   return nxt[v][x];
 }
 
-void add(const vector<ll> diff, ll x) {
+void add(const vector<ll>& diff, ll x) {
   ll v = 0;
   for (auto i : diff) {
     v = get_nxt(v, i);
@@ -91,7 +95,7 @@ ll try_find(const vector<ll>& diff) {
     if (!nxt[v].count(i)) {
       return -1;
     }
-    v = nxt[v];
+    v = nxt[v][i];
   }
   return lst[v];
 }
@@ -107,15 +111,22 @@ int main(int argc, char** argv) {
   rep(i, n) {
     cin >> a[i];
   }
+  // printvec(a);
   vector<ll> a1(n);
   vector<ll> a2(n);
   rep(i, n) {
-    a1[i] = (a[i] >> 15); // The left 15 bit
-    ll mask = ~(-1 << 15);
-    a2[i] = a[i] & mask;
+    a1[i] = (a[i] >> 15); // The left 15 bit.
+    a2[i] = a[i] ^ (a1[i] << 15); // The right 15 bit.
   }
+  // printvec(a1);
+  // printvec(a2);
+
   rep(i, 1<<15) {
     vector<ll> d = get_diff(a1, i);
+    // cout << "i: " << i << endl;
+    // printvec(d);
+    // cout << "t: " << t << endl;
+    // cout << "lst[t]: " << lst[t] << endl;
     add(d, i);
   }
 
@@ -124,7 +135,12 @@ int main(int argc, char** argv) {
     rep(j, n-1) {
       d[j] *= -1;
     }
+    // cout << "i: " << i << endl;
+    // printvec(d);
+
     ll x = try_find(d);
+    // cout << "x: " << x << endl;
+    // cout << "lst: "; printvec(lst);
     if (x != -1) {
       ll res = (x << 15) ^ i;
       cout << res << endl;
