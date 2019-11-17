30e98266e3bb38e52f2fc630bd77e0d87c2eb44e
Nao Minami <south37777@gmail.com>
2019-11-17 13:41:55 +0900

    Update

--- round600/e/e_wrong.cpp
+++ round600/e/e_wrong.cpp
@@ -54,6 +54,53 @@ int main(int argc, char** argv) {
   ios_base::sync_with_stdio(false);
   //cout << setprecision(10) << fixed;
 
-  ll n;
-  cin >> n;
+  ll n, m;
+  cin >> n >> m;
+  vector<P> lights;
+  rep(i, n) {
+    ll x, s;
+    cin >> x >> s;
+    lights.emplace_back(x, s);
+  }
+  sort(all(lights));
+  rep(i, n) {
+    ll x = lights[i].fr;
+    ll s = lights[i].sc;
+    cout << "(x, s)" << "("<<x<<", "<<s<<")" << endl;
+  }
+
+  // Now, lights are sorted in increasing order.
+  vector<ll> dist(n+1); // dist[i] .. the distance between lights[i], lights[i-1]. dist[0] is between the boundary.
+  dist[0] = max(0LL, lights[0].fr - lights[0].sc);
+  for (int i = 1; i < n; ++i) {
+    dist[i] = max(0LL, (lights[i].fr - lights[i-1].fr) - (lights[i].sc + lights[i-1].sc));
+  }
+  dist[n] = max(0LL, (m - lights[n-1].fr) - lights[n-1].sc);
+  cout << "dist: "; printvec(dist);
+
+  // Now, we decide coins from left.
+  ll ans = 0;
+  if (dist[0] > 0) {
+    rep(i, n) {
+      if (dist[i] > 0) {
+        ans += dist[i]; // Use dist[i] conins
+        dist[i+1] = max(0LL, dist[i+1] - dist[i]);
+      }
+    }
+    if (dist[n] > 0) {
+      ans += dist[n];
+    }
+  } else {
+    for (int i = n; i >= 1; --i) {
+      if (dist[i] > 0) {
+        ans += dist[i]; // Use dist[i] conins
+        dist[i-1] = max(0LL, dist[i-1] - dist[i]);
+      }
+    }
+    if (dist[0] > 0) {
+      ans += dist[0];
+    }
+  }
+
+  cout << ans << endl;
 }
