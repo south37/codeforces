6516a342d5bd595da90b8912736c5432da0c2076
Nao Minami <south37777@gmail.com>
2020-02-23 17:06:12 +0900

    Add round620/b

--- round620/b/b.cpp
+++ round620/b/b.cpp
@@ -48,7 +48,47 @@ typedef double D;
 const ll INF = 1e9;
 const ll MOD = 1000000007;  // 1e9 + 7
 
-void solve() {
+bool isPalindrom(const string& s) {
+  map<char, int> hist;
+  for (char c : s) {
+    ++hist[c];
+  }
+  bool oddFound = false;
+  for (auto& p : hist) {
+    if (p.second % 2 == 1) {
+      if (oddFound) { return false; } // odd can be seen only once
+      oddFound = true;
+    }
+  }
+  return true;
+}
+
+string toPalindrom(const string& s) {
+  int n = s.size();
+  // If n is odd, only 1 character is odd. other is even.
+  map<char, int> hist;
+  for (char c : s) {
+    ++hist[c];
+  }
+  char odd;
+  string ans;
+  for (auto& p : hist) {
+    if (p.second % 2 == 1) {
+      odd = p.first;
+      if (p.second > 1) {
+        ans += string(p.second/2, p.first);
+      }
+    } else {
+      ans += string(p.second/2, p.first);
+    }
+  }
+  string suffix = ans;
+  reverse(all(suffix));
+  if (n % 2 == 1) {
+    ans += odd;
+  }
+  ans += suffix;
+  return ans;
 }
 
 int main(int argc, char** argv) {
@@ -57,9 +97,47 @@ int main(int argc, char** argv) {
   ios_base::sync_with_stdio(false);
   //cout << setprecision(10) << fixed;
 
-  ll t;
-  cin >> t;
-  rep(i, t) {
-    solve();
+  ll n, m;
+  cin >> n >> m;
+  map<string, int> cnts;
+  vector<string> inputs(n);
+  rep(i, n) {
+    cin >> inputs[i];
+    sort(all(inputs[i]));
+    ++cnts[inputs[i]];
   }
+  // Here, we use pair in each string.
+
+  map<string, int> ansCnts;
+  for (auto& p : cnts) {
+    if (p.second >= 2) { // cnt >= 2
+      int usedCnt = (p.second / 2); // should be even.
+      ansCnts[p.first] = usedCnt;
+      cnts[p.first] -= usedCnt * 2;
+    }
+  }
+  // Here, ansCnts has the information about ans.
+  string ansPrefix;
+  for (auto& p : ansCnts) {
+    int cnt = p.second;
+    rep(iter, cnt) {
+      ansPrefix += p.first;
+    }
+  }
+  // Here, ansPrefix is the prefix. suffix is the reversed one.
+
+  string ans = ansPrefix;
+  for (auto& p : cnts) {
+    if (p.second > 0) {
+      if (isPalindrom(p.first)) {
+        ans += toPalindrom(p.first);
+        break; // we use only one palindrom
+      }
+    }
+  }
+
+  reverse(all(ansPrefix));
+  ans += ansPrefix;
+  cout << ans.size() << endl;
+  cout << ans << endl;
 }
