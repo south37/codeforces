9a320c94b55a13627d52676c02a8351569894bc9
Nao Minami <south37777@gmail.com>
2020-02-23 16:43:18 +0900

    Update template

--- ruby/template.rb
+++ ruby/template.rb
@@ -1,3 +1,14 @@
-N = gets.chomp.to_i
-A, B, C = gets.chomp.split.map(&:to_i)
-# S = gets.chomp.chars
+def main
+  t = gets.chomp.to_i
+  t.times { solve }
+end
+
+def solve
+  n = gets.chomp.to_i
+  a, b, c = gets.chomp.split.map(&:to_i)
+  # S = gets.chomp.chars
+  p n
+  p [a, b, c]
+end
+
+main
