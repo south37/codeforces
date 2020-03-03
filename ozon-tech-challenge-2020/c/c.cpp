cd3e5f78d1eb15017286ce6e462ef11df17b0bb4
Nao Minami <south37777@gmail.com>
2020-03-04 00:33:25 +0900

    Update

--- ozon-tech-challenge-2020/c/c.cpp
+++ ozon-tech-challenge-2020/c/c.cpp
@@ -46,10 +46,73 @@ typedef tuple<ll, ll, ll> triple;
 typedef double D;
 
 const ll INF = 1e9;
-const ll MOD = 1000000007;  // 1e9 + 7
+ll MOD;
 
-void solve() {
-}
+// Mod int
+// cf. https://www.youtube.com/watch?v=1Z6ofKN03_Y
+struct mint {
+  ll x;
+  mint(ll x = 0) : x((x + MOD) % MOD) {}
+  mint& operator+= (const mint a) {
+    if ((x += a.x) >= MOD) x %= MOD;
+    return *this;
+  }
+  mint operator+ (const mint a) const {
+    mint res(*this);
+    return res += a;
+  }
+  mint& operator-= (const mint a) {
+    if ((x += MOD - a.x) >= MOD) x %= MOD;
+    return *this;
+  }
+  mint operator- (const mint a) const {
+    mint res(*this);
+    return res -= a;
+  }
+  mint& operator*= (const mint a) {
+    (x *= a.x) %= MOD;
+    return *this;
+  }
+  mint operator* (const mint a) const {
+    mint res(*this);
+    return res *= a;
+  }
+  mint pow(ll t) const {
+    if (!t) { return 1; }
+    mint a = pow(t >> 1);
+    a *= a;
+    if (t & 1) a *= *this;
+    return a;
+  }
+
+  // for prime mod
+  mint inv() const {
+    return pow(MOD-2);
+  }
+  mint& operator/= (const mint a) {
+    return (*this) *= a.inv();
+  }
+  mint operator/ (const mint a) const {
+    mint res(*this);
+    return res /= a;
+  }
+};
+
+// int main(int argc, char** argv) {
+//   // int p;
+//   // cin >> p;
+//
+//   MOD = 13;
+//   mint p(10);
+//   cout << (p + 15).x << endl;   // 12 (25 % 13)
+//   cout << (p - 15).x << endl;   // 8  (-5 % 13)
+//   cout << (p * 2).x << endl;    // 7  (20 % 13)
+//   cout << (p.pow(3)).x << endl; // 12 (1000 % 13)
+//   cout << (p / 3).x << endl;    // 12 (12 * 3 = 10 (36 % 13))
+//
+//   mint p2(-3);
+//   cout << p2.x << endl; // 10 (-3 % 13)
+// }
 
 int main(int argc, char** argv) {
   cin.tie(NULL);
@@ -57,9 +120,56 @@ int main(int argc, char** argv) {
   ios_base::sync_with_stdio(false);
   //cout << setprecision(10) << fixed;
 
-  ll t;
-  cin >> t;
-  rep(i, t) {
-    solve();
+  ll n, m;
+  cin >> n >> m;
+  MOD = m;
+
+  vector<ll> a(n);
+  rep(i, n) {
+    cin >> a[i];
   }
+  sort(all(a));
+  // printvec(a);
+
+  // Here, we calculate the a[i] mod m
+  map<ll, ll> Rcnts; // the map of counts.
+  rep(i, n) {
+    ll c = a[i] % m;
+    ++Rcnts[c];
+  }
+  // printmap(Rcnts);
+
+  // map<ll, ll> Lcnts; // the map of counts.
+  mint ans(1);
+  // scan from a0
+  rep(i, n) {
+    mint contrib(1);
+    ll c = a[i] % m;
+    --Rcnts[c];
+    if (Rcnts[c] == 0) { // reach to 0
+      Rcnts.erase(c);
+    }
+    // printmap(Rcnts);
+    // cout << "c: "<< c << endl;
+
+    // from right
+    for (const pair<ll, ll>& p : Rcnts) {
+      mint contrib2(p.first - c);
+      contrib2 *= p.second;
+      contrib *= contrib2;
+    }
+
+    // // from left
+    // for (const pair<ll, ll>& p : Lcnts) {
+    //   mint contrib2(c - p.first);
+    //   contrib2 *= p.second;
+    //   contrib *= contrib2;
+    // }
+
+    ans *= contrib;
+
+    // ++Lcnts[c];
+  }
+
+  cout << ans.x << endl;
 }
