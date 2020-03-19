b4011582feba16419633f61f80fa84b9447a76b5
Nao Minami <south37777@gmail.com>
2020-03-20 01:43:39 +0900

    Update

--- global-round-7/b/b.cpp
+++ global-round-7/b/b.cpp
@@ -50,18 +50,36 @@ typedef double D;
 const ll INF = 1e9;
 const ll MOD = 1000000007;  // 1e9 + 7
 
-void solve() {
-}
-
 int main(int argc, char** argv) {
   cin.tie(NULL);
   cout.tie(NULL);
   ios_base::sync_with_stdio(false);
   //cout << setprecision(10) << fixed;
 
-  ll t;
-  cin >> t;
-  rep(i, t) {
-    solve();
+  ll n;
+  cin >> n;
+  vector<ll> b(n);
+  rep(i, n) {
+    cin >> b[i];
   }
+  vector<ll> x(n+1);
+  x[0] = 0;
+
+  // a[i] = b[i] + x[i].
+  vector<ll> a(n); // answer
+  // a[0] = b[0]; // a[0] = b[0] + x[0]
+  ll maxA = a[0];
+  for (int i = 0; i < n; ++i) {
+    a[i] = b[i] + x[i];
+    maxA = max(maxA, a[i]);
+    x[i+1] = maxA;
+  }
+  // Here, all a is calculated
+  rep(i, n) {
+    cout << a[i];
+    if (i < n-1) {
+      cout << " ";
+    }
+  }
+  cout << endl;
 }
