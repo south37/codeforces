64997c17a4d0c7baa1a77330b68a5a7bb37780c5
Nao Minami <south37777@gmail.com>
2020-03-21 17:58:09 +0900

    Add grobal-round-7/e

--- global-round-7/e/e.cpp
+++ global-round-7/e/e.cpp
@@ -50,8 +50,56 @@ typedef double D;
 const ll INF = 1e9;
 const ll MOD = 1000000007;  // 1e9 + 7
 
-void solve() {
-}
+// Segment Tree
+class SegTree {
+public:
+  SegTree(int _n) {
+    n = 1;
+    while (n < _n) { n *= 2; }
+    add = vector<int>(2 * n - 1);
+    t = vector<int>(2 * n - 1);
+  }
+
+  void update(int k, int x) {
+    _update(0, 0, k, 0, n, x);
+  }
+
+  // target: [l, r). area: [tl, tr).
+  void _update(int v, int l, int r, int tl, int tr, int x) {
+    if (tl >= r || tr <= l) { return; } // not in range
+    if (tl >= l && tr <= r) {
+      update_one(v, x);
+    } else {
+      push(v);
+      _update(v*2+1, l, r, tl, (tl+tr)/2, x);
+      _update(v*2+1, l, r, (tl+tr)/2, tr, x);
+      t[v] = min(t[v*2+1], t[v*2+2]);
+    }
+  }
+
+  // We need only the result of whole area, so return t[0].
+  int query() {
+    return t[0];
+  }
+
+private:
+  void update_one(int v, int x) {
+    add[v] += x;
+    t[v] += x;
+  }
+
+  void push(int v) {
+    if (add[v]) {
+      update_one(v*2+1, add[v]);
+      update_one(v*2+2, add[v]);
+      add[v] = 0;
+    }
+  }
+
+  int n; // The size of source data. The power of 2.
+  vector<int> add;
+  vector<int> t;
+};
 
 int main(int argc, char** argv) {
   cin.tie(NULL);
@@ -59,9 +107,38 @@ int main(int argc, char** argv) {
   ios_base::sync_with_stdio(false);
   //cout << setprecision(10) << fixed;
 
-  ll t;
-  cin >> t;
-  rep(i, t) {
-    solve();
+  ll n;
+  cin >> n;
+  vector<ll> p(n);
+  vector<ll> revP(n); // idx of the value in p.
+  vector<ll> q(n);
+  rep(i, n) {
+    cin >> p[i];
+    revP[p[i]] = i;
   }
+  rep(i, n) {
+    cin >> q[i];
+    --q[i]; // 0-indexed
+  }
+
+  SegTree st(n);
+  auto decrease = [&] (int x) {
+    st.update(revP[x] + 1, -1);
+    if (st.query() >= 0) {
+      return true;
+    } else {
+      st.update(revP[x] + 1, 1); // revert
+      return false;
+    }
+  };
+  int x = n;
+  rep(i, n) {
+    while (decrease(x)) { --x; }
+    cout << x;
+    if (i < n-1) {
+      cout << " ";
+    }
+    st.update(q[i] + 1, 1);
+  }
+  cout << endl;
 }
