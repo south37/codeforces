ffcd2f72a6dfa8d0ff0c1958bacaf45673a517d5
Nao Minami <south37777@gmail.com>
2020-03-24 00:17:24 +0900

    Update

--- edu_84/b/b.cpp
+++ edu_84/b/b.cpp
@@ -51,6 +51,56 @@ const ll INF = 1e9;
 const ll MOD = 1000000007;  // 1e9 + 7
 
 void solve() {
+  ll n;
+  cin >> n;
+  set<ll> st; // represents remaining prince
+  vector<ll> unused_dauter;
+  rep(i, n) { st.insert(i); }
+  rep(i, n) {
+    ll k;
+    cin >> k;
+    vector<ll> targets(k); // target princes
+    rep(i, k) {
+      cin >> targets[i];
+      --targets[i]; // 0-indexed
+    }
+
+    // Check the presence of prince
+    auto stTarget = st.begin();
+    bool isFound = false;
+    rep(j, k) {
+      if (targets[j] < *stTarget) { continue; } // skip small one.
+      // Here, targets[j] >= stTarget
+      if (targets[j] == *stTarget) { // found
+        st.erase(stTarget);
+        isFound = true;
+        break;
+      }
+      // Here, targets[j] > stTarget
+      auto it = st.lower_bound(targets[j]);
+      if (it == st.end()) { // not found
+        break;
+      }
+      if (targets[j] == *it) { // found
+        st.erase(it);
+        isFound = true;
+        break;
+      } else { // targets[j] > *it
+        stTarget = it;
+      }
+    }
+    if (!isFound) {
+      unused_dauter.push_back(i);
+    }
+  }
+
+  // Here, check the count
+  if (st.size() == 0) { // all st is used
+    cout << "OPTIMAL" << endl;
+  } else {
+    cout << "IMPROVE" << endl;
+    cout << unused_dauter[0] + 1 << " " << (*st.begin()) + 1<< endl;
+  }
 }
 
 int main(int argc, char** argv) {
