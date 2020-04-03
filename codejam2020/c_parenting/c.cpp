914134b292909dd440a09d034a24aa42505b75cb
Nao Minami <south37777@gmail.com>
2020-04-04 08:39:46 +0900

    Add c

---
+++ codejam2020/c_parenting/c.cpp
@@ -0,0 +1,121 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <random>
+#include <string.h>
+#include <set>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+#define COUT(x) cout << #x << " = " << (x) << " (L" << __LINE__ << ")" << endl
+
+template<class T> void printvec(const vector<T>& v) {
+  for (auto x : v) { cout << x << " "; }
+  cout << endl;
+}
+template<class T> void printtree(const vector< vector<T> >& tree) {
+  for (long long i = 0; i < tree.size(); ++i) {
+    cout << i + 1 << ": "; printvec(tree[i]);
+  }
+}
+template<class T, class U> void printmap(const map<T, U>& mp) {
+  for (auto x : mp) { cout << x.first << "=>" << x.second << endl; }
+}
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+#define rep(i, n) for(ll i = 0; i < n; ++i)
+#define all(s) s.begin(), s.end()
+#define fr first
+#define sc second
+#define mp make_pair
+#define pb push_back
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> P;
+typedef tuple<ll, ll, ll> triple;
+typedef double D;
+
+const ll INF = 1e9;
+const ll MOD = 1000000007;  // 1e9 + 7
+
+void solve() {
+  ll n;
+  cin >> n;
+  vector<triple> times(n);
+  rep(i, n) {
+    ll s, e;
+    cin >> s >> e;
+    times[i] = { s, e, i };
+  }
+  sort(all(times));
+  // Here, we add times in greedy way
+  ll j = 0, c = 0; // end time of j and c
+
+  vector<ll> ans(n, -1); // 0 => j, 1 => c
+
+  rep(iter, n) {
+    ll s, e, i;
+    tie(s, e, i) = times[iter];
+
+    if (j < c) {
+      // Here, we should try c at first
+      if (c <= s) { // c is ok
+        ans[i] = 1;
+        c = e;
+      } else if (j <= s) { // j is ok
+        ans[i] = 0;
+        j = e;
+      } else {
+        cout << "IMPOSSIBLE";
+        return;
+      }
+    } else {
+      // Here, we should try j at first
+      if (j <= s) { // j is ok
+        ans[i] = 0;
+        j = e;
+      } else if (c <= s) { // c is ok
+        ans[i] = 1;
+        c = e;
+      } else {
+        cout << "IMPOSSIBLE";
+        return;
+      }
+    }
+  }
+  // Here, we reached valid
+  rep(i, n) {
+    if (ans[i] == 0) {
+      cout << "J";
+    } else { // ans[i] == 1
+      cout << "C";
+    }
+  }
+}
+
+int main(int argc, char** argv) {
+  cin.tie(NULL);
+  cout.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  //cout << setprecision(10) << fixed;
+
+  ll t;
+  cin >> t;
+  rep(i, t) {
+    cout << "Case #" << (i+1) << ": ";
+    solve();
+    cout << endl;
+  }
+}
