bd5a7026ba3056146a8f3566233c0334e20f9a45
Nao Minami <south37777@gmail.com>
2020-04-11 11:15:46 +0900

    Add c

--- codejam2020-1a/c/c.cpp
+++ codejam2020-1a/c/c.cpp
@@ -51,6 +51,76 @@ const ll INF = 1e9;
 const ll MOD = 1000000007;  // 1e9 + 7
 
 void solve() {
+  ll h, w;
+  cin >> h >> w;
+  vector<vector<ll>> mat(h, vector<ll>(w));
+  rep(r, h) {
+    rep(c, w) {
+      cin >> mat[r][c];
+    }
+  }
+  // Here, mat has all information
+  // we calculate in greedy way
+  ll ans = 0;
+  while(true) {
+    bool changed = false;
+    ll roundSum = 0;
+    // calculate sum
+    rep(r, h) rep(c, w) { roundSum += mat[r][c]; }
+    ans += roundSum;
+
+    vector<vector<bool>> eliminated(h, vector<bool>(w));
+
+    // eiliminate
+    rep(r, h) rep(c, w) {
+      if (mat[r][c] == 0) { continue; } // skip eliminated onem
+
+      // Here, we search all neibers;
+      // find small r
+      vector<ll> founds;
+      for (int rr = r-1; rr >= 0; --rr) {
+        if (mat[rr][c] > 0) { // found
+          founds.push_back(mat[rr][c]);
+        }
+      }
+      for (int rr = r+1; rr < h; ++rr) {
+        if (mat[rr][c] > 0) { // found
+          founds.push_back(mat[rr][c]);
+        }
+      }
+      for (int cc = c-1; cc >= 0; --cc) {
+        if (mat[r][cc] > 0) { // found
+          founds.push_back(mat[r][cc]);
+        }
+      }
+      for (int cc = c+1; cc < w; ++cc) {
+        if (mat[r][cc] > 0) { // found
+          founds.push_back(mat[r][cc]);
+        }
+      }
+      // Here, founds has neibors
+      if (founds.size() == 0) { continue; } // skip if no neiber
+      ll average = 0;
+      for (ll found : founds) { average += found; }
+      if (average > founds.size() * mat[r][c]) { // too small
+        // cout << "(r,c): " << r << "," << c << endl;
+        // cout << "average: " << average << endl;
+        changed = true;
+        eliminated[r][c] = true;
+      }
+    }
+    if (!changed) {
+      break;
+    } else {
+      // ans += roundSum;
+      rep(r, h) rep(c, w) {
+        if (eliminated[r][c]) {
+          mat[r][c] = 0;
+        }
+      }
+    }
+  }
+  cout << ans;
 }
 
 int main(int argc, char** argv) {
