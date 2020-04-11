5807872515a8e8291edec6d2bec7340d7addaea3
Nao Minami <south37777@gmail.com>
2020-04-11 14:51:01 +0900

    Update b

--- codejam2020-1a/b/b.cpp
+++ codejam2020-1a/b/b.cpp
@@ -50,151 +50,80 @@ typedef double D;
 const ll INF = 1e9;
 const ll MOD = 1000000007;  // 1e9 + 7
 
-void solve_for_small_n(ll n) {
-  if (n == 1) {
-    cout << "1 1" << endl;
-    return;
-  } else if (n == 2) {
-    cout << "1 1" << endl;
-    cout << "2 1" << endl;
-    return;
-  } else if (n == 3) {
-    cout << "1 1" << endl;
-    cout << "2 1" << endl;
-    cout << "3 1" << endl;
-    return;
-  } else { // n >= 4
-    cout << "1 1" << endl;
-    cout << "2 1" << endl;
-    cout << "3 2" << endl; // (2, 1)
-    ll remain = n - 4;
-    // Here, our position is (2, 1)
-    // start from here, and increment in greedy way
-    ll r = 2;
-    while (remain > 0) {
-      // Here, we do "(r, 1)" pair. It is rC1 = r.
-      ++r;
-      if (remain >= r) {
-        remain -= r;
-        cout << r + 1 << " " << 2 << endl;
-      } else { // reached to small remaining
-        break;
-      }
+// dir: 0 .. left to right, 1 .. right to left
+void printrow(int r, int dir) {
+  int w = r + 1;
+  if (dir == 0) { // left to right
+    rep(c, w) {
+      cout << r+1 << " " << c+1 << endl;
     }
-    --r; // decrease.
-    // Here, we do small remaining
-    while (remain > 0) {
-      cout << r + 1 << " " << 1 << endl; // (r, 0)
-      --remain;
-      ++r;
+  } else { // right to left
+    for (int c = w-1; c >= 0; --c) {
+      cout << r+1 << " " << c+1 << endl;
     }
+  }
+}
+
+void solve_in_greedy(ll n) {
+  ll remain = n;
+  ll r = 0;
+  while (remain > 0) {
+    cout << r+1 << " " << 1 << endl;
+    --remain;
+    ++r;
   }
 }
 
 void solve() {
   ll n;
   cin >> n;
-  // Here, we calculate all case.
-  // start from (0, 0). (n, k) is nCk.
 
-  if (n <= 1000) {
-    solve_for_small_n(n);
+  if (n <= 31) {
+    // solve in greedy way
+    solve_in_greedy(n);
     return;
   }
-
-  vector<vector<ll>> pascal(34); // 33C16 > 1e9. we need only this
-  pascal[0].resize(1);
-  pascal[0][0] = 1;
-  rep(r, 34) {
-    pascal[r].resize(r+1);
-    pascal[r][0] = 1;
-    pascal[r][pascal[r].size()-1] = 1;
-    for (int k = 1; k < pascal[r].size()-1; ++k) {
-      pascal[r][k] = pascal[r-1][k-1] + pascal[r-1][k];
+  vector<bool> used(31); // used[i] .. using 2^i
+  {
+    ll remain = n - 31; // decrease for moving
+    for (ll i = 30; i >= 0; --i) {
+      if (remain >= (1ll<<i)) {
+        remain -= 1ll<<i;
+        used[i] = true;
+      }
     }
   }
+  // Here, all information is calclated
+  cout << "used:"; printvec(used);
 
-  // For Debug
-  // rep(r, 34) {
-  //   printvec(pascal[r]);
-  // }
-
-  // vector<ll> fact(41); // 40C20 = 1e11. we need only this
-  // fact[0] = 1;
-  // for (ll i = 1; i <= 40; ++i) { fact[i] = fact[i-1] * i; }
-  // printvec(fact);
-
-  // Here, we consider large n.
-  // we fetch center number in greedy way.
   ll remain = n;
-  ll r = 0;
-  while (remain > 0) {
-    assert(r <= 33);
-
-    ll k = r/2;
-
-    // Here, we calculate rCk
-    // ll cont = comb[r];
-    // cont *= fact[r];
-    // cont /= fact[k];
-    // cont /= fact[r-k];
-    ll cont = pascal[r][k];
-    // cout << "remain: " << remain << endl;
-    // cout << r<<"C"<<k<<": " << cont << endl;
-    if (remain < cont * 2) {
-      break;
-    } else { // remain >= cont
-      cout << r + 1 << " " << k + 1 << endl;
-      remain -= cont;
-    }
-    ++r;
-  }
-  // Here, r is not printed yet.
-  --r;
-  ll k = r / 2;
-  // Here, we get contribution from (r-1,k-1)
-  while (remain > 0) {
-    --r;
-    --k;
-    if (k >= 0) {
-      // Calc rCk
-      ll cont = pascal[r][k];
-      // ll cont = comb[r];
-      // cont *= fact[r];
-      // cont /= fact[k];
-      // cont /= fact[r-k];
-
-      assert(remain >= cont);
-      // cout << "remain: " << remain << endl;
-      // cout << r<<"C"<<k<<": " << cont << endl;
-      cout << r + 1 << " " << k + 1 << endl;
-      remain -= cont;
-    } else { // k < 0
-      break;
+  int dir = 0;
+  for (int r = 0; r <= 30; ++r) {
+    if (used[r]) {
+      printrow(r, dir);
+      dir ^= 1; // flip
+      remain -= (1ll<<r);
+    } else { // not used. only move
+      if (dir == 0) { // next, left to right. so should print left
+        cout << r+1 << " " << 1 << endl;
+      } else {
+        cout << r+1 << " " << r+1 << endl;
+      }
+      --remain;
     }
   }
-  ++r;
-  // Here, r is remaining, but k reached to 0.
-  // we start from (r, 0) and go along (r+i, 0)
+
+  // Here, remain may remaining
+  ll r = 30;
   while (remain > 0) {
-    if (remain >= r) {
-      cout << r + 1 << " 2" << endl;
-      remain -= r;
-      ++r;
+    if (dir == 0) {
+      cout << r+1 << " " << 1 << endl;
     } else {
-      break;
+      cout << r+1 << " " << r+1 << endl;
     }
-  }
-  // Here, r is not printed yet.
-  --r;
-  while (remain > 0) {
-    cout << r + 1<< " 1" << endl;
-    remain -= 1;
+    --remain;
     ++r;
   }
-
-  // Here, remain == 0
-  return;
 }
 
 int main(int argc, char** argv) {
