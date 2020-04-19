f0f44317c58922832066c4f8d79d364b352d14ea
Nao Minami <south37777@gmail.com>
2020-04-20 02:58:09 +0900

    Update

--- codejam2020-1b/b/b.cpp
+++ codejam2020-1b/b/b.cpp
@@ -50,10 +50,101 @@ typedef double D;
 const ll INF = 1e9;
 const ll MOD = 1000000007;  // 1e9 + 7
 
+string checkResult(ll x, ll y) {
+  // cerr << "testcase: " << testcase << endl;
+  // cerr << "idx: " << idx << endl;
+  // cerr << flush;
+
+  cout << x << " " << y << endl;
+  cout << std::flush;
+  string res;
+  cin >> res; // '0' or '1'
+  return res;
+}
+
+ll N = 1e9;
+
+ll calcLeft(ll y) {
+  ll left;
+  {
+    ll l = -N-1;
+    ll r = -N+105;
+    // found the boundary of [f, t]
+    while (r - l > 1) {
+      ll m = (r+l) / 2;
+      string res = checkResult(m, y);
+      if (res == "HIT") {
+        r = m;
+      } else {
+        l = m;
+      }
+    }
+    // Here, r is ok
+    left = r;
+  }
+  return left;
+}
+
+ll calcRight(ll y) {
+    ll right;
+    {
+      ll l = N-105;
+      ll r = N+1;
+      // found the boundary of [t, f]
+      while (r - l > 1) {
+        ll m = (r+l) / 2;
+        string res = checkResult(m, y);
+        if (res == "HIT") {
+          l = m;
+        } else {
+          r = m;
+        }
+      }
+      // Here, r is ok
+      right = l;
+    }
+    return right;
+}
+
+// return the diameter at y
+ll g(ll y) {
+  ll left = calcLeft(y);
+  ll right = calcRight(y);
+  return right - left;
+}
+
+void hit_center(ll y) {
+  ll left = calcLeft(y);
+  ll right = calcRight(y);
+  ll x = (left + right)/2;
+  string res = checkResult(x, y);
+  if (res == "CENTER") {
+    return;
+  } else {
+    // Here, invalid
+    abort();
+  }
+}
+
 void solve() {
-  ll n;
-  cin >> n;
-  cout << n << endl;
+  ll a, b;
+  cin >> a >> b;
+  // check left in binary search
+  // Do 3-min in y-direction
+
+  ll l = -105;
+  ll r = 105;
+  rep(iter, 10) {
+    ll c1 = (l*2+r)/3;
+    ll c2 = (l+r*2)/3;
+    if (g(c1) > g(c2)) {
+      l = c1;
+    } else {
+      r = c2;
+    }
+  }
+  // Here, l is the result
+  hit_center(l);
 }
 
 int main(int argc, char** argv) {
