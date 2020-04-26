cf45629f4b4409f9351b319037c04bdbec85c4d3
Nao Minami <south37777@gmail.com>
2020-04-27 01:36:32 +0900

    Update

--- edu_86/d/d_wip.cpp
+++ edu_86/d/d_wip.cpp
@@ -51,9 +51,60 @@ const ll INF = 1e9;
 const ll MOD = 1000000007;  // 1e9 + 7
 
 void solve() {
-  ll n;
-  cin >> n;
-  cout << n << endl;
+  ll n, k;
+  cin >> n >> k;
+  vector<ll> m(n);
+  vector<ll> c(k+1);
+  reverse(all(c));
+
+  rep(i, n) {
+    cin >> m[i];
+  }
+  rep(j, k) {
+    cin >> c[j+1];
+  }
+
+  vector<ll> cnt(k+1);
+  rep(i, n) {
+    ++cnt[m[i]];
+  }
+  // Here, cnt is calculated
+  set<P> st;
+  rep(j, k+1) {
+    if (cnt[j] > 0) {
+      st.insert({ j, cnt[j] });
+    }
+  }
+
+  ll ans = 0;
+  while (!st.empty()) {
+    ++ans;
+    // add in greedy way
+    ll cnt = 0;
+    while (true) {
+      ll id = upper_bound(all(c), cnt) - c.begin();
+      if (id == n) { // not found
+        break;
+      }
+      // Here, c[i] >= cnt
+      ll v = n - id; // smallest value
+
+      auto p = st.lower_bound({ v, -1 }); // smallest value in s
+      if (p == st.begin() && p->first != v) { //
+        break;
+      }
+      if (p == st.end()) { // v is large
+        p = prev(st.end());
+      }
+      if (p->second == 1) { // should be erased
+        st.erase(p);
+      } else {
+        --(*p).second;
+      }
+      ++cnt;
+    }
+  }
+  cout << ans << endl;
 }
 
 int main(int argc, char** argv) {
