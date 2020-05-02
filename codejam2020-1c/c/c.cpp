8cb9ed51d64ad481d82927fac630f674ef381290
Nao Minami <south37777@gmail.com>
2020-05-02 19:41:42 +0900

    Add c

--- codejam2020-1c/c/c.cpp
+++ codejam2020-1c/c/c.cpp
@@ -51,8 +51,55 @@ const ll INF = 1e9;
 const ll MOD = 1000000007;  // 1e9 + 7
 
 void solve() {
-  ll n;
-  cin >> n;
+  ll n, d;
+  cin >> n >> d;
+  vector<ll> a(n);
+  rep(i,n) {
+    cin >> a[i];
+  }
+  map<ll,ll> cnts;
+  rep(i,n) {
+    ++cnts[a[i]];
+  }
+
+  // try 0
+  {
+    // Here, we try cut0, cut1, cut2
+    for (auto& p : cnts) {
+      if (p.second >= d) { // ok
+        cout << 0 << endl;
+        return;
+      }
+    }
+  }
+  // try 1
+  {
+    if (d <= 2) { // slice to 2.
+      cout << 1 << endl;
+      return;
+    }
+    if (cnts.size() > 1) {
+      for (auto& p : cnts) {
+        if (p.second+1 >= d) { // ok
+          cout << 1 << endl;
+          return;
+        }
+      }
+
+      for (auto& p : cnts) {
+        ll now = 2*p.first; // p is divided
+        // cout << "p.first: " << p.first << endl;
+        // cout << "p.second: " << p.second << endl;
+        // cout << "d: " << d << endl;
+
+        if (cnts.find(now) != cnts.end() && p.second + 2 >= d) {
+          cout << 1 << endl;
+          return;
+        }
+      }
+    }
+  }
+  cout << 2 << endl;
 }
 
 int main(int argc, char** argv) {
@@ -66,6 +113,5 @@ int main(int argc, char** argv) {
   rep(i, t) {
     cout << "Case #" << (i+1) << ": ";
     solve();
-    cout << endl;
   }
 }
